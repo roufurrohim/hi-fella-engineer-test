2f5bc5d7fe44a31fcd5de37e7f3acb17bf7f8862
teamdml1 <teamdmlone@protonmail.com>
2025-03-21 06:29:31 +0700

    Fixing sass

--- src/components/progressbar.tsx
+++ src/components/progressbar.tsx
@@ -22,7 +22,7 @@ const ProgressBar = ({ percentage = 0, height = 20, backgroundColor = '#CECECE',
   return (
     <div className={styles['progress-bar-container']} style={{ height: `${height}px`, backgroundColor, borderRadius: `${radius}px` }}>
       <div 
-        className={`${styles['progress-bar-fill']} ${animate ? 'animate' : ''}`} 
+        className={`${styles['progress-bar-fill']} ${animate ? styles['animate'] : ''}`} 
         style={{ 
           width: `${percentage}%`, 
           backgroundColor: fillColor,
